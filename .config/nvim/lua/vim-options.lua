096563b6d748956ef99caf1bbb09f3f676bd52ef
Philip <44866668+px-d@users.noreply.github.com>
2023-12-30 16:40:21 +0100

    updated

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,7 +1,9 @@
 vim.o.relativenumber = true
 
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 vim.g.mapleader = " "
+
+-- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
