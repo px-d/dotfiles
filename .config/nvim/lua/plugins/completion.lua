5076e1063429b9c8888b5c7a462b846ab5b69206
Philip <44866668+px-d@users.noreply.github.com>
2024-02-07 12:37:10 +0100

    updated configs
    linearmouse changed
    completions for nvim new keybinds

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -37,7 +37,7 @@ return {
                     end,
                 },
                 mapping = {
-                    ["<S-j>"] = cmp.mapping(function(fallback)
+                    ["<S-down>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
                         elseif luasnip.expand_or_jumpable() then
@@ -48,7 +48,7 @@ return {
                             fallback()
                         end
                     end, { "i", "s" }),
-                    ["<S-k>"] = cmp.mapping(function(fallback)
+                    ["<S-up>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
                         elseif luasnip.jumpable(-1) then
